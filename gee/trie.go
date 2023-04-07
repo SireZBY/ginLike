1b637460b0c02121afeb65a33901ea526b8bf4e8
boyang.zhang <boyang.zhang@supwisdom.com>
2023-04-07 14:18:45 +0800

    day3: 更新router类中前缀树相关功能

--- gee/trie.go
+++ gee/trie.go
@@ -38,7 +38,7 @@ func (n *node) insert(pattern string, parts []string, height int) {
 
 	part := parts[height]
 	child := n.matchChild(part)
-	if child != nil {
+	if child == nil {
 		child = &node{part: part, isWild: part[0] == ':' || part[0] == '*'}
 		n.children = append(n.children, child)
 	}
