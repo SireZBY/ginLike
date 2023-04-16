2278e2a12f47fd988e9d3637d57f684e5608bd2f
boyang.zhang <boyang.zhang@supwisdom.com>
2023-04-16 16:54:34 +0800

    day4: 添加路由分组

--- gee/gee.go
+++ gee/gee.go
@@ -1,33 +1,61 @@
 package gee
 
 import (
+	"log"
 	"net/http"
 )
 
 type HandlerFunc func(c *Context)
 
+type RouterGroup struct {
+	prefix      string
+	middlewares []HandlerFunc // 路由分组-中间件支持
+	parent      *RouterGroup
+	engine      *Engine // 路由分组共享一个gee的Engine实例
+}
+
 type Engine struct {
+	*RouterGroup
 	router *router
+	groups []*RouterGroup
 }
 
 // New 构造器函数
 func New() *Engine {
-	return &Engine{router: newRouter()}
+	//return &Engine{router: newRouter()}
+	engine := &Engine{router: newRouter()}
+	engine.RouterGroup = &RouterGroup{engine: engine}
+	engine.groups = []*RouterGroup{engine.RouterGroup}
+	return engine
 }
 
-func (engine *Engine) addRoute(method string, pattern string, handler HandlerFunc) {
+func (group *RouterGroup) Group(prefix string) *RouterGroup {
+	engine := group.engine
+	newGroup := &RouterGroup{
+		prefix: group.prefix + prefix,
+		parent: group,
+		engine: engine,
+	}
+	engine.groups = append(engine.groups, newGroup)
+	return newGroup
+}
+
+func (group *RouterGroup) addRoute(method string, comp string, handler HandlerFunc) {
 	//key := method + "-" + pattern
 	//// 添加路由响应事件
 	//engine.router[key] = handler
-	engine.router.addRoute(method, pattern, handler)
+	//engine.router.addRoute(method, pattern, handler)
+	pattern := group.prefix + comp
+	log.Printf("Route %4s - %s", method, pattern)
+	group.engine.router.addRoute(method, pattern, handler)
 }
 
-func (engine *Engine) GET(pattern string, handler HandlerFunc) {
-	engine.addRoute("GET", pattern, handler)
+func (group *RouterGroup) GET(pattern string, handler HandlerFunc) {
+	group.addRoute("GET", pattern, handler)
 }
 
-func (engine *Engine) POST(pattern string, handler HandlerFunc) {
-	engine.addRoute("POST", pattern, handler)
+func (group *RouterGroup) POST(pattern string, handler HandlerFunc) {
+	group.addRoute("POST", pattern, handler)
 }
 
 func (engine *Engine) Run(addr string) (err error) {
